ac6abbba941e4bfb5f3ef6cc0c3ff77bc9086621
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-04 08:20:13 -0700

    httpsse.EventWriter

--- eventwriter.go
+++ eventwriter.go
@@ -4,6 +4,13 @@ import (
 	"io"
 )
 
+// EventWriter represents an event that can be written to.
+//
+// Use Write to write the event-data.
+//
+// Use SetEventName to set the event-name.
+//
+// Use SetEventID to set the event-ID.
 type EventWriter interface {
 	io.Writer
 	SetEventName(string)
