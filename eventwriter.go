6d9c5af5a944931b860741d96b5d7eff3b1450ff
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-04 08:33:41 -0700

    httpsse.EventWriter

--- eventwriter.go
+++ eventwriter.go
@@ -11,6 +11,12 @@ import (
 // Use SetEventName to set the event-name.
 //
 // Use SetEventID to set the event-ID.
+//
+// You can give anything that is an EventWriter to [Client].Decode
+//
+// [Event] is an EventWriter, and can be passed to [Client].Decode
+//
+// But you can also use other types, including creating your own, that are EventWriter, and pass it to [Client].Decode
 type EventWriter interface {
 	io.Writer
 	SetEventName(string)
